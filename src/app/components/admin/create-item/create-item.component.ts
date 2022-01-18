063a6996083d74becf775d469a10a5acf48b59c9
ServandoL <swyswy214@gmail.com>
2022-01-17 20:17:50 -0600

    update create-item component

--- src/app/components/admin/create-item/create-item.component.ts
+++ src/app/components/admin/create-item/create-item.component.ts
@@ -1,15 +1,47 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { IProduct } from '../../../models/product.model';
 
 @Component({
   selector: 'app-create-item',
   templateUrl: './create-item.component.html',
-  styleUrls: ['./create-item.component.css']
+  styleUrls: ['./create-item.component.css'],
 })
-export class CreateItemComponent implements OnInit {
+export class CreateItemComponent {
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  newProduct!: IProduct;
+
+  createItemForm = new FormGroup({
+    productName: new FormControl('', [Validators.required]),
+    productPrice: new FormControl('', [Validators.required]),
+    productCategory: new FormControl('', [Validators.required]),
+    productBrand: new FormControl('', [Validators.required]),
+    productStock: new FormControl(0, [Validators.required]),
+    productImage: new FormControl(''),
+  });
+
+  get productName() {
+    return this.createItemForm.get('productName');
+  }
+  get productPrice() {
+    return this.createItemForm.get('productPrice');
+  }
+  get productCategory() {
+    return this.createItemForm.get('productCategory');
+  }
+  get productBrand() {
+    return this.createItemForm.get('productBrand');
+  }
+  get productStock() {
+    return this.createItemForm.get('productStock');
+  }
+  get productImage() {
+    return this.createItemForm.get('productImage');
   }
 
+  onSubmit() {
+    alert('submitted');
+  }
 }
--- src/app/models/product.model.ts
+++ src/app/models/product.model.ts
@@ -1,14 +1,22 @@
-import { ObjectId } from "mongodb";
+import { ObjectId } from 'mongodb';
 
 export interface IProduct {
-  _id: string | ObjectId | null
-  productName: string
-  price: number
-  category: string
-  brand: string
-  stock: number
-  imageUrl?: string
-  isDeleted?: boolean
-  deleted_by?: string
-  deleted_date?: string
+  _id: string | ObjectId | null;
+  productName: string;
+  price: number;
+  category: string;
+  brand: string;
+  stock: number;
+  imageUrl?: string;
+  isDeleted?: boolean;
+  deleted_by?: string;
+  deleted_date?: string;
+}
+
+export interface ICategory {
+  category: string;
+}
+
+export interface IBrand {
+  brand: string;
 }
