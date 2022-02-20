032a101e33c1b7243e600d07f10e4e0a12b5b348
ServandoL <swyswy214@gmail.com>
2022-02-19 21:58:32 -0600

    add user, delete user queries

--- src/app/services/state/users/users.actions.ts
+++ src/app/services/state/users/users.actions.ts
@@ -8,11 +8,35 @@ const LoadCurrentUserSuccessAction = '[User] Load Current User Success';
 const LoadCurrentUserFailureAction = '[User] Load Current User Failure';
 const LoadUsers = '[User] Load Users';
 const GetCurrentUserAction = '[User] Get Current User';
+const CreateUserAction = '[User] Create User';
+const CreateUserActionSuccess = '[User] Create User Success';
+const CreateUserActionFailure = '[User] Create User Failure';
 
 export const setCurrentUser = createAction(
   CurrentUserAction,
-  props<{ email: string, password: string }>()
+  props<{ email: string; password: string }>()
 );
+
+export const createUser = createAction(
+  CreateUserAction,
+  props<{
+    email: string;
+    password: string;
+    firstName: string;
+    lastName: string;
+    isAdming: boolean;
+  }>()
+);
+
+export const createUserSuccess = createAction(
+  CreateUserActionSuccess,
+  props<{ message: string, success: boolean }>()
+)
+
+export const CreateUserFailure = createAction(
+  CreateUserActionFailure,
+  props<{ error: string }>()
+)
 
 export const clearCurrentUser = createAction(ClearCurrentUserAction);
 export const initializeCurrentUser = createAction(InitCurrnetUserAction);
